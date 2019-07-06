1a1ac471c4aa6c349f5aeadda53d1acbcfe3deb0
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-07-07 08:08:08 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>タスク一覧</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:checked="task.done">
+        {{ task.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+ export default {
+  name: 'App',
+  computed: {
+    tasks () {
+      console.log(this.$store.state.tasks[0].id)
+      return this.$store.state.tasks
+    },
+  },
+  
+ }
+ 
+</script>
---
+++ src/store.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    tasks: [
+      {
+        id: 1,
+        name: '牛乳を買う',
+        done: false
+      },
+      {
+        id: 2,
+        name: 'vueの本を買う',
+        done: true
+      }
+    ],
+  },
+})
+
