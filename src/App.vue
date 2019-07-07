49cbc2b5f27ec8c840a1e8c49729f132826fe022
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-07-07 12:20:39 +0900

    タスク追加機能の実装

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,46 @@
     <h2>タスク一覧</h2>
     <ul>
       <li v-for="task in tasks" v-bind:key="task.id">
-        <input type="checkbox" v-bind:checked="task.done">
+        <input type="checkbox" v-bind:checked="task.done"
+          v-on:change="toggleTaskStatus(task)">
         {{ task.name }}
       </li>
     </ul>
+
+    <form v-on:submit.prevent="addTask">
+      <input type="text" v-model="newTaskName" placeholder="新しいタスク">
+    </form>
   </div>
 </template>
 
 <script>
  export default {
-  name: 'App',
+   data: function() {
+     return {
+       newTaskName: '',
+     }
+   },
   computed: {
-    tasks () {
-      console.log(this.$store.state.tasks[0].id)
+    tasks: function () {
       return this.$store.state.tasks
     },
   },
-  
+  methods: {
+    // タスクの追加
+    addTask: function() {
+      // 'addTask'mutationをコミット
+      this.$store.commit('addTask', {
+        name: this.newTaskName,
+      })
+      this.newTaskName = ''
+    },
+    // タスク完了状態の更新
+    toggleTaskStatus: function(task) {
+      // 'toggleTaskStatus'mutationをコミット
+      this.$store.commit('toggleTaskStatus', {
+        id: task.id
+      })
+    },
+  },
  }
- 
 </script>
--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // タスク初期のstate
     tasks: [
       {
         id: 1,
@@ -17,6 +18,30 @@ export default new Vuex.Store({
         done: true
       }
     ],
+    // 次に追加するタスク　今回は決め打ち
+    nextTaskId: 3,
+  },
+  mutations: {
+    // タスクの追加
+    addTask: function(state, { name }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        done: false
+      })
+      // 次に追加されるタスクに追加されるidを更新
+      state.nextTaskId++
+      console.log()
+    },
+    // タスクの完了状態を更新
+    toggleTaskStatus: function(state, { id }){
+      const filtered = state.tasks.filter(task => {
+        return task.id === id
+      })
+      filtered.forEach(task => {
+        task.done = !task.done
+      })
+    },
   },
 })
 
